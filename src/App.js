16389c907f216f81f9b444f499f48e067376ae80
Satyajit Gokhale <twisterintwist@gmail.com>
2024-06-05 12:20:34 -0400

    after transformation

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
+import { memo } from "react";
 import React, { useState } from 'react';
 import TaskList from './TaskList';
-
 export const TaskContext = React.createContext();
-
-const App = () => {
+const App = memo(() => {
   const [tasks, setTasks] = useState([]);
   const [newTaskText, setNewTaskText] = useState('');
-
   const handleAddTask = () => {
     if (newTaskText.trim() !== '') {
-      setTasks([...tasks, { text: newTaskText, completed: false }]);
+      setTasks([...tasks, {
+        text: newTaskText,
+        completed: false
+      }]);
       setNewTaskText('');
     }
   };
-
-  const handleNewTaskTextChange = (event) => {
+  const handleNewTaskTextChange = event => {
     setNewTaskText(event.target.value);
   };
-
-  return (
-    <TaskContext.Provider value={{ tasks, setTasks }}>
+  return <TaskContext.Provider value={{
+    tasks,
+    setTasks
+  }}>
       <div>
         <h1>Lista de tareas</h1>
         <div>
-          <input
-            type="text"
-            value={newTaskText}
-            onChange={handleNewTaskTextChange}
-            id="taskText"
-          />
+          <input type="text" value={newTaskText} onChange={handleNewTaskTextChange} id="taskText" />
           <button onClick={handleAddTask}>Agregar tarea</button>
         </div>
         <TaskList />
       </div>
-    </TaskContext.Provider>
-  );
-};
-
+    </TaskContext.Provider>;
+});
 export default App;
