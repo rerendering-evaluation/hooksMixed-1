787ac1770b4a6906ac9bf2e7daa7039baa34401b
Satyajit Gokhale <twisterintwist@gmail.com>
2024-06-07 13:24:57 -0400

    after transformation

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
+import { useRef } from "react";
+import { memo } from "react";
 import React, { useState } from 'react';
 import TaskList from './TaskList';
-
 export const TaskContext = React.createContext();
-
-const App = () => {
+const App = memo(() => {
   const [tasks, setTasks] = useState([]);
-  const [newTaskText, setNewTaskText] = useState('');
-
+  const newTaskText = useRef('');
   const handleAddTask = () => {
-    if (newTaskText.trim() !== '') {
-      setTasks([...tasks, { text: newTaskText, completed: false }]);
-      setNewTaskText('');
+    if (newTaskText.current.value.trim() !== '') {
+      setTasks([...tasks, {
+        text: newTaskText.current.value,
+        completed: false
+      }]);
+      newTaskText.current.value = '';
     }
   };
-
-  const handleNewTaskTextChange = (event) => {
-    setNewTaskText(event.target.value);
+  const handleNewTaskTextChange = event => {
+    newTaskText.current.value = event.target.value;
   };
-
-  return (
-    <TaskContext.Provider value={{ tasks, setTasks }}>
+  return <TaskContext.Provider value={{
+    tasks,
+    setTasks
+  }}>
       <div>
         <h1>Lista de tareas</h1>
         <div>
-          <input
-            type="text"
-            value={newTaskText}
-            onChange={handleNewTaskTextChange}
-            id="taskText"
-          />
+          <input type="text" ref={newTaskText} id="taskText" />
           <button onClick={handleAddTask}>Agregar tarea</button>
         </div>
         <TaskList />
       </div>
-    </TaskContext.Provider>
-  );
-};
-
+    </TaskContext.Provider>;
+});
 export default App;
